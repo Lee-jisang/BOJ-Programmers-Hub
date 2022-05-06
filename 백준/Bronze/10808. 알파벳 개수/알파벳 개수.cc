4e315394769ebfd0bd70b3f276f0c5c9011f74aa
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-05-06 15:39:50 +0900

    [Bronze II] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -8,8 +8,7 @@ int main() {
 
 	string s;
 	cin >> s;
-
-	char a[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	
 	int b[26] = { 0, };
 	for (int i = 0; i < s.length(); i++) {
 		b[int(s[i]) - 97]++;
