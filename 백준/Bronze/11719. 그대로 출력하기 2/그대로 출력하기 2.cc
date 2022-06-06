ac81a4d5b97864ec035e2791703d4b16ec97f156
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-06-06 22:31:48 +0900

    [Bronze II] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    string s;
+   
+    while (getline(cin, s)) 
+        cout << s << "\n"; 
+
+}
