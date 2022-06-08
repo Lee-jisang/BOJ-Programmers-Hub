b474512eb2ffc6257a1eca92a777eb2b897553b5
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-06-08 11:21:37 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	
+	int a;
+	cin >> a;
+	string s;
+	for (int i = 0; i < a; i++) {
+		cin >> s;
+		cout << s[0] << s[s.length() - 1] << "\n";
+
+	}
+		
+}
