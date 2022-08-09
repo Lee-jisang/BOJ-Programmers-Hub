8aa035b978774099b02d72884dd10789caf14c5a
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-09 09:51:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.20 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    map<string,int> m;
+    unordered_map<string,int> m;
     string answer="";
     for(int i=0; i<participant.size(); i++) m[participant[i]]+=1; 
     for(int i=0; i<completion.size(); i++) m[completion[i]]-=1; 
