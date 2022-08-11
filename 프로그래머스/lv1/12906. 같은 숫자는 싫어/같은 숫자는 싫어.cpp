b7dafbbf79646bf2b1ec08518ac76be5078fed2d
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-11 16:21:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 108.51 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,23 +1,13 @@
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-vector<int> solution(vector<int> arr)
+vector<int> solution(vector<int> arr) 
 {
-    vector<int> answer;
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] == arr[i + 1]) {
-            if(i+1==arr.size()){
-                answer.push_back(arr[i]);
-                break;
-            }
-            continue;
-        }
-        else {
-            answer.push_back(arr[i]);
-        }
-    }
+
+    arr.erase(unique(arr.begin(), arr.end()),arr.end());
+
+    vector<int> answer = arr;
     return answer;
 }
-
