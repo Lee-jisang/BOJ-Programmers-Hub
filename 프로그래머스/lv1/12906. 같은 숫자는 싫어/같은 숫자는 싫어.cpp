9d04e6499f15731f2e08ccdfd1c2574242b8c760
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-11 16:22:02 +0900

    Update 같은 숫자는 싫어.cpp

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -11,3 +11,28 @@ vector<int> solution(vector<int> arr)
     vector<int> answer = arr;
     return answer;
 }
+
+
+// #include <vector>
+// #include <iostream>
+
+// using namespace std;
+
+// vector<int> solution(vector<int> arr)
+// {
+//     vector<int> answer;
+//     for (int i = 0; i < arr.size(); i++) {
+//         if (arr[i] == arr[i + 1]) {
+//             if(i+1==arr.size()){
+//                 answer.push_back(arr[i]);
+//                 break;
+//             }
+//             continue;
+//         }
+//         else {
+//             answer.push_back(arr[i]);
+//         }
+//     }
+//     return answer;
+// }
+
