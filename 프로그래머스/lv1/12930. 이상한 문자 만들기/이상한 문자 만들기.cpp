44584c5a4b5a3d5f732db86cc60c9191915a75db
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-12 12:26:09 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+string solution(string s) {
+    int idx = 0;
+    for(int i=0; i<s.size(); i++){
+        
+        if(s[i] == ' '){
+            idx=0;
+            continue; 
+        }
+        
+        if(idx%2==1){
+            s[i]=tolower(s[i]);
+        }
+        else{
+            s[i]=toupper(s[i]);
+        }
+        idx++;
+    }
+    
+    return s;
+}
