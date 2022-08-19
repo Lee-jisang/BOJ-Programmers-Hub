775bb2d6fe318d6c1ebc8bba43ed4667dea4c3a1
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-19 23:19:57 +0900

    [Bronze II] Title: 그릇, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/7567. 그릇/그릇.cc
+++ 백준/Bronze/7567. 그릇/그릇.cc
@@ -10,35 +10,23 @@ int main() {
 	string s;
 	cin >> s;
 	int sum = 10;
-	if (s.find(")") == string::npos) {//)가 없으면
-		cout << s.length() * 5 + 5;
-	}
-	else if (s.find("(") == string::npos) {//(가 없으면
-		cout << s.length() * 5 + 5;
-	}
-	else {
-		for (int i = 0; i < s.length()-1; i++) {
-			if (s[i] == ')') {
-				if (s[i + 1] == ')') {//))
-					sum += 5;
-				}
-				else {//)(
-					sum += 10;
-				}
+	for (int i = 0; i < s.length() - 1; i++) {
+		if (s[i] == ')') {
+			if (s[i + 1] == ')') {//))
+				sum += 5;
 			}
-			else {
-				if (s[i + 1] == ')') {//()
-					sum += 10;
-				}
-				else {//((
-					sum += 5;
-				}
+			else {//)(
+				sum += 10;
 			}
 		}
-		cout << sum;
+		else {
+			if (s[i + 1] == ')') {//()
+				sum += 10;
+			}
+			else {//((
+				sum += 5;
+			}
+		}
 	}
-
-
-
-
+	cout << sum;
 }
