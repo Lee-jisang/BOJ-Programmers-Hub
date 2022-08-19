e59e9e6160366fb142d16b4a1d4599156603db09
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-19 10:43:50 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string s) {
+    if(s.length()==1){
+        return 1;
+    }
+    int k=0;
+    int min=1001;
+    for(int i=s.length()/2; i>=1; i--){   
+        vector<string> v;
+        for(int j=0; j<=s.length()-1; j=j+i){  
+            v.push_back(s.substr(j,i));
+        }     
+        
+        for(int j=0; j<v.size()-1; j++){  
+            for(int k=j+1; k<v.size(); k++){
+                if(v[j]==v[k]) v[k]="*";
+                else break;  
+            } 
+        }
+     
+        int sum=0;
+        for(int j=0; j<v.size(); j++){        
+            int cnt=0;
+            for(int k=j+1; k<v.size(); k++){
+                if(v[j]!=v[k] && v[k]=="*"){
+                    cnt++;
+                    continue;
+                }else{
+                    break;
+                }            
+            }      
+            if(1<=cnt && cnt<=8){
+                sum += v[j].length()+1;
+            }else if(9<=cnt && cnt<=98){
+                sum += v[j].length()+2;
+            }else if(99<=cnt && cnt<=998){
+                sum += v[j].length()+3;  
+            }else if(cnt==999){
+                sum += v[j].length()+4;
+            }else{//cnt가 0일때
+                if(v[j]!="*") sum += v[j].length();                  
+            }     
+        }
+        if(min>sum) min=sum;         
+    }
+    return min;
+}
