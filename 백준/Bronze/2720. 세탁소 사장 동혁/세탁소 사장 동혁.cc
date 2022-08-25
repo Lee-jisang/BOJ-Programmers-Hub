356720cbb1bf9dfc293dec6b425bfe65d4d73304
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-25 15:48:22 +0900

    Update 세탁소 사장 동혁.cc

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -7,15 +7,12 @@ int main() {
 	int a;
 	int b[4] = {25,10,5,1};
 
-	for (int i = 0; i < T; i++) {	
+	for (int i = 0; i < T; i++) {
 		cin >> a;
-		cout << a / b[0] << " ";
-		a = a % b[0];
-		cout << a / b[1] << " ";
-		a = a % b[1];
-		cout << a / b[2] << " ";
-		a = a % b[2];
-		cout << a / b[3] << "\n";
-		a = a % b[3];
+		for (int j = 0; j < 4; j++) {
+			cout << a / b[j] << " ";
+			a = a % b[j];
+		}	
+		cout << "\n";		
 	}
 }
