3ad25e7cb5c2900584681f5f1507f5b8c35f5a69
LJS <76837780+Lee-jisang@users.noreply.github.com>
2022-08-25 16:55:09 +0900

    [Gold IV] Title: 문자열 폭발, Time: 48 ms, Memory: 7796 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+
+int main() {
+
+    string s, bomb;
+    cin >> s >> bomb;
+
+    string answer = "";
+
+    int bombIdx = bomb.length() - 1; //1
+    for (int i = 0; i < s.length(); i++) {
+
+        answer += s[i];
+
+        if (answer[answer.length() - 1] == bomb[bombIdx]) {//   ==4
+
+            if (answer.length() >= bomb.length()) {
+                int cnt = 1;
+
+                for (int j = bombIdx - 1; j >= 0; j--) { //j=0
+                    if (answer[answer.length() - (bombIdx - j) - 1] == bomb[j]) {
+                        cnt++;
+                    }
+                }
+
+                if (bomb.length() == cnt) {
+                    for (int b = 0; b < bomb.length(); b++) {
+                        answer.pop_back();
+                    }
+                }
+
+            }
+        }
+    }
+
+    if (answer.length() > 0) {
+        cout << answer << endl;
+    }
+    else {
+        cout << "FRULA" << endl;
+    }
+
+
+    return 0;
+}
