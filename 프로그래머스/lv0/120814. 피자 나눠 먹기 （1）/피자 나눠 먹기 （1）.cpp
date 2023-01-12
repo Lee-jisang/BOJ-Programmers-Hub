3e7692c1b42ef1d8be0f20cc614e6d0eac97c1c6
LJS <76837780+Lee-jisang@users.noreply.github.com>
2023-01-12 09:54:17 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    
+    int answer = 0;
+    if(n%7==0){
+        return n/7;
+    }else{
+        return n/7+1;
+    }
+    
+}
