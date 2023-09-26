375ad74b60a27cf121ce8f3a60ea0dd992846a32
LJS <76837780+Lee-jisang@users.noreply.github.com>
2023-09-26 17:58:22 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public List<String> solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<answer.length; i++){
+            if(answer[i].equals("")) continue;
+            list.add(answer[i]);
+        }
+        return list;
+    }
+}
