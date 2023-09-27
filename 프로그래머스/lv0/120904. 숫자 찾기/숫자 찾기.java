18818fc9ecabdb9565c73964484b20073a4e8535
LJS <76837780+Lee-jisang@users.noreply.github.com>
2023-09-27 15:27:07 +0900

    [level 0] Title: 숫자 찾기, Time: 7.63 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String n = num+"";
-        for(int i=0; i<n.length(); i++){
-            if(n.charAt(i)==(k+'0'))
-                return i+1;
-        }
-        return -1;
+        return ("-" + num).indexOf(k+"");
     }
 }
