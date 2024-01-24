d3cc1eae9f7029d96527978d8e487192c036c6d5
LJS <76837780+Lee-jisang@users.noreply.github.com>
2024-01-24 10:54:41 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
-        
+          
         for(int i=0; i<emergency.length; i++){
             for(int j=0; j<emergency.length; j++){
                 if(j==i) continue;
                 if(emergency[i]<emergency[j])
                     answer[i]++;
-            }
+            } 
         }
         for(int i=0; i<answer.length; i++)
             answer[i]++;
