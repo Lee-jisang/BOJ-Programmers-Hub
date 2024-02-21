ab089f8905bb5e56276a67573a146dd4e8885214
LJS <76837780+Lee-jisang@users.noreply.github.com>
2024-02-21 16:56:09 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+      
+    int a, b;
+    
+    for(int i=0; i<T; i++){
+        cin >> a >> b;       
+        cout << a+b << endl;      
+    }
+
+
+ 
+  
+}
