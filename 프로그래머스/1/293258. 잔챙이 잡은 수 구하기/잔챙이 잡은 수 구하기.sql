2497bacb02bd2605606e66b2c1172b6c7790b114
LJS <76837780+Lee-jisang@users.noreply.github.com>
2024-09-04 16:07:53 +0900

    [level 1] Title: 잔챙이 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
+++ 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
@@ -1,3 +1,8 @@
-SELECT COUNT(*) AS FISH_COUNT
-FROM FISH_INFO
-WHERE LENGTH <= '10' || LENGTH IS NULL
+# SELECT COUNT(*) AS FISH_COUNT
+# FROM FISH_INFO
+# WHERE LENGTH <= '10' || LENGTH IS NULL
+
+select count(*) as FISH_COUNT
+from fish_info
+where length <= 10.0 || length is null #float type 일때 조건
+;
