0a3d81862db994a0f83b319799e102a73e448271
LJS <76837780+Lee-jisang@users.noreply.github.com>
2024-09-12 10:49:20 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,14 +1,16 @@
 # -- 코드를 작성해주세요
-select a.id as ID
-from (
-select id, parent_id
-from ECOLI_DATA
-where parent_id is not null
-) a left outer join ECOLI_DATA b
-on a.parent_id = b.id 
-left outer join ECOLI_DATA c on b.parent_id = c.id
-where b.parent_id is not null
-  and c.parent_id is null
-order by ID
+# select a.id as ID
+# from(
+# select id, parent_id
+# from ECOLI_DATA
+# where parent_id is not null
+# ) a left outer join ECOLI_DATA b
+# on a.parent_id = b.id
+# where b.parent_id is not null
+# order by a.id
 
-
+SELECT a.id AS ID
+FROM ECOLI_DATA a
+JOIN ECOLI_DATA b ON a.parent_id = b.id
+WHERE b.parent_id IS NOT NULL
+ORDER BY a.id;
